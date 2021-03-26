ac2b6319a66ac1743c4e71b8a6ab7e2498f68b2d
Lannivi <on.a.collision.course@yandex.ru>
2021-03-27 01:30:25 +0400

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  str = String(str);
+
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+  let additionStr = String(options.addition);
+
+ if (options.separator === undefined) {
+    options.separator = '+';
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = '1';
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = '1';
+  }
+
+
+  let addStrResult = '';
+  let resultAll = '';
+  for (let i = 1; i <= options.additionRepeatTimes; i++) {
+    i < options.additionRepeatTimes
+      ? addStrResult += additionStr + options.additionSeparator
+      : addStrResult += additionStr;
+    }
+
+  for (let j = 1; j <= options.repeatTimes; j++) {
+    j < options.repeatTimes
+      ? resultAll += str + addStrResult + options.separator
+      : resultAll += str + addStrResult;
+  }
+
+  return resultAll;
 };
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  
+  if(!Array.isArray(arr)) {
+    throw Error;
+  }
+
+  let result = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+        i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (arr[i-2] !== '--discard-next') {
+        result.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (arr.length > i + 2) {
+        result.push(arr[i + 1]);
+      }
+
+    } else if (arr[i] === '--double-prev') {
+      if (i > 0 && arr[i - 2] !== '--discard-next')
+        result.push(arr[i - 1]);
+      
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
